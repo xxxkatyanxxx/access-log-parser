4e1a71eda32451eca016d567048e33df0b3d7dd1
xxxkatyanxxx <xxxkatyanxxx@gmail.com>
2025-11-03 14:23:50 +0700

    Добавлена папка папка META-INF

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст и нажмите <Enter>: ");
+        String text = scanner.nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
