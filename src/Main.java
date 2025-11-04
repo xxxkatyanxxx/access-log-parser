42e4d14386aee354fd6789d0bcbcd6c152dc3ea4
xxxkatyanxxx <xxxkatyanxxx@gmail.com>
2025-11-04 11:58:49 +0700

    Добавлены операции расчета суммы, разности, произведения и частного вводимых чисел

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = scanner.nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+        System.out.println("Введите число");
+        int number1 = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите число");
+        int number2 = new Scanner(System.in).nextInt();
+
+
+        System.out.println(number1+number2);
+        System.out.println(number1-number2);
+        System.out.println(number1*number2);
+        System.out.println((double) number1/number2);
+        scanner.close();
+
+
+
     }
 }
